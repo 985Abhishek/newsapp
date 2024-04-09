ac74cbc283ca5bcc9a70f5fde21449af206fee6e
Abhishek Dhange <abhishekdhange@gmail.com>
2024-04-09 23:40:13 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -2,95 +2,100 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
 export default class News extends Component {
-  articles = [
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-    {
-      "source": {
-      "id": "polygon",
-      "name": "Polygon"
-      },
-      "author": "Michael McWhertor",
-      "title": "Overwatch 2 is allowed to be fun this week",
-      "description": "The April Fools’ Day 2024 patch for Overwatch 2 is more than just googly eyes: Balance Overwatch in the arcade is a great place to try out weird new hero changes.",
-      "url": "https://www.polygon.com/24118989/overwatch-2-april-fools-2024-balance-patch",
-      "urlToImage": "https://cdn.vox-cdn.com/thumbor/FkzuLtN64RkfH-pMlt9hdHsWY1c=/0x92:1800x1034/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/25367376/overwatc_2_afd.jpg",
-      "publishedAt": "2024-04-02T17:17:27Z",
-      "content": "Image: Blizzard Entertainment\r\n\n \n\n Blizzard brought some silly new ideas to the shooter for April Fools’ Day A new patch for Overwatch 2 rolled out Monday that promises a “really, really, really bal… [+14815 chars]"
-      },
-      {
-        "source": {
-        "id": null,
-        "name": "TheWrap"
-        },
-        "author": "Eileen AJ Connelly",
-        "title": "Taylor Swift Joins Forbes’ Billionaires Rankings",
-        "description": "The \"Gold Rush\" singer is the first to make the magazine's list of the world's richest people on just music and ticket sales\nThe post Taylor Swift Joins Forbes’ Billionaires Rankings appeared first on TheWrap.",
-        "url": "https://www.thewrap.com/taylor-swift-billionaire-rankings/",
-        "urlToImage": "https://www.thewrap.com/wp-content/uploads/2023/06/Tswift-Eras-Lover.jpg",
-        "publishedAt": "2024-04-02T17:15:23Z",
-        "content": "Taylor Swift joined the ranks of celebrity billionaires Tuesday when she became the most famous newcomer on Forbes Magazines annual list of the richest people in the world. \r\nThe Mastermind singer ma… [+2888 chars]"
-        },
-  ];
+
   constructor() {
     super();
     console.log("Hello I ma a constructor from News component");
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
+      page: 1,
     };
   }
+
+  async componentDidMount() {
+    console.log("cdm");
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e083cc59881248dbb3b8569a32dec661";
+    let data = await fetch(url);
+    console.log(data);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
+
+  }
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e083cc59881248dbb3b8569a32dec661&page=${this.state.page - 1}&pageSize-20`;
+    let data = await fetch(url);
+    console.log(data);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    })
+  }
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 >Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e083cc59881248dbb3b8569a32dec661&page=${this.state.page + 1}&pageSize-20`;
+    let data = await fetch(url);
+    console.log(data);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles,
+    })
+  }
+  }
+
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1 className="text-center">NewsMonkey - Top Headlines</h1>
         <div className="row">
           {this.state.articles.map((element) => {
             return (
-              <div className="col-md-4" key = {element.url}>
-                <Newsitem 
-                  title={element.title.slice(0,45)}
-                  description={element.description.slice(0,88)}
+              <div className="col-md-4" key={element.url}>
+                <Newsitem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 88) : ""
+                  }
                   imageUrl={element.urlToImage}
-                  newsUrl= {element.url}
+                  newsUrl={element.url}
                 />
               </div>
             );
           })}
         </div>
+        <div className="container d-flex justify-content-between">
+        <button
+  type="button"
+  disabled={this.state.page <= 1}
+  class="btn btn-dark"
+  onClick={this.handlePrevClick}
+>
+  {" "}
+  &larr; Previous{" "}
+</button>
+          <button
+            type="button"
+            diabled ={(this.state.page + 1 >Math.ceil(this.state.totalResults/20))}
+            class="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            {" "}
+            Next &rarr;
+          </button>
+        </div>
       </div>
-    );
+    )
   }
 }
+
+
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -6,11 +6,11 @@ export class Newsitem extends Component {
     return (
       <div className = "my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://variety.com/wp-content/uploads/2016/08/reuters3201-e1536889239712.jpg?w=1000&h=563&crop=1": imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target = "_blank" className="btn btn-sm btn-primary">
+            <a rel="noreferrer" href={newsUrl} target = "_blank" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
